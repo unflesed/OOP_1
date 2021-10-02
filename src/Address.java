62134ba63ba5cf54c903fd4792dcf0a5f9e6fa09
Anatoliy Morozov <anatoly.marozaw@gmail.com>
2021-10-02 12:43:23 +0300

    add files OOP_1

---
+++ src/Address.java
@@ -0,0 +1,44 @@
+public class Address {
+    public int index;
+    public String country;
+    public String city;
+    public String street;
+    public int house;
+    public int apartment;
+    public int getIndex() {
+        return index;
+    }
+    public String getCountry() {
+        return country;
+    }
+    public String getCity() {
+        return city;
+    }
+    public String getStreet() {
+        return street;
+    }
+    public int getHouse() {
+        return house;
+    }
+    public int getApartment() {
+        return apartment;
+    }
+    public void setIndex(int index) {
+        this.index = index;
+    }
+    public void setCountry(String country) {
+        this.country = country;
+    }
+    public void setCity(String city) {
+        this.city = city;
+    }
+    public void setStreet(String street) {
+        this.street = street;
+    }
+    public void setHouse(int house) {
+        this.house = house;
+    }
+    public void setApartment(int apartment) {
+        this.apartment = apartment;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        Address address = new Address();
+        address.setIndex(1);
+        address.setCountry("Russia");
+        address.setCity("Moscow");
+        address.setStreet("Pushkinskay st.");
+        address.setHouse(1);
+        address.setApartment(11);
+        System.out.println(address.getIndex() + " " + address.getCountry() + " " +address.getCity() + " "
+        + address.getStreet() + " " + address.getHouse() + " " + address.getApartment());
+
+    }
+}
