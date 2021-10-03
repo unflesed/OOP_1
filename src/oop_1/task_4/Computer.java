9909d4028aeef16c695e1c57b0c43cdba5230e3d
Anatoliy Morozov <anatoly.marozaw@gmail.com>
2021-10-03 13:44:40 +0300

    task_4 done

---
+++ src/oop_1/task_4/Computer.java
@@ -0,0 +1,6 @@
+package oop_1.task_4;
+
+public class Computer {
+    String monitor;
+    String systemUnit;
+}
---
+++ src/oop_1/task_4/Main.java
@@ -0,0 +1,10 @@
+package oop_1.task_4;
+
+public class Main {
+    public static void main(String[] args) {
+        Computer[] computers = new Computer[5];
+        for (int i = 0; i < 5; i++) {
+            computers[i] = new Computer();
+        }
+    }
+}
